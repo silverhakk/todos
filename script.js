5d622b0c4453b53ceba10774f93faa751ee1eb91
silverhakk <pato4shot@gmail.com>
2026-06-25 18:10:42 +0100

    div

--- script.js
+++ script.js
@@ -2,12 +2,31 @@
 // 1. ASYNC FUNCTION to fetch data from JSONPlaceholder
 // -----------------------------------------------------------
 async function geTodos() {
-  const statusEl =document.getElementById(('status'))
-  const listellement=document.getElementById('todoList') 
+  const statusEl = document.getElementById("status");
+  const listellement = document.getElementById("todoList");
+  statusEl.textContent = "fetching todos ... ";
+  listellement.innerHTML.trimRight = "";
   try {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    if (!res.ok) throw new Error("unable to fetch data");
     const data = await res.json();
-    console.log(data);
+    const todos = data.slice(0, 10);
+    if (todos.length === 0) {
+      listellement.innerHTML = `
+  <li>no todos at the moment</li>
+  `;
+    }
+
+    todos.forEach((todo) => {
+      const li = document.createElement("li");
+      li.textContent = todo.title;
+      li.className = "todo-item";
+      if (todo.completed) {
+        li.style.textDecoration = "line-through";
+      }
+
+      listellement.appendChild(li);
+    });
   } catch (error) {
     console.log(error);
   }
